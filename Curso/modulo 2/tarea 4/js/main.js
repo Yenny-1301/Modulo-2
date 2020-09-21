2a10023f714b35c13457529c077d1ca1339f431f
Yenny-1301 <yennyfergf03@gmail.com>
2020-09-21 11:50:27 -0300

    senate tabla y filtros

--- Curso/modulo 2/tarea 4/js/main.js
+++ Curso/modulo 2/tarea 4/js/main.js
@@ -1,26 +1,36 @@
-// nombre del header: X-API-Key
-// api key cBmV3a8yZ4WsCVDLZmmTsPksTFKrLO4bwrqtt200
+const app= new Vue({
+	el:"#app",
+	data:{
+		api:"https://api.propublica.org/congress/v1/113/senate/members.json",
+		i:{headers:{"X-API-Key":"cBmV3a8yZ4WsCVDLZmmTsPksTFKrLO4bwrqtt200"}},
+		members:[],
+		party:["D","R","ID"],
+		states:[],
+		stateSelected:"All",
+	},
+	methods:{
+		options:function(){
+			for (let  i= 0; i <this.members.length ; i++) {
+				if (!this.states.includes(this.members[i].state)) {
+					this.states.push(this.members[i].state)
+				}	
+			}
+			return [...this.states].sort()	
+		},	
+	},
+	computed:{
+		filtered:function(){
+		return this.members.filter(member=> this.party.includes(member.party) && (this.stateSelected==member.state || this.stateSelected=="All"))
+		},
+	},
+	created: function(){
+		fetch(this.api,this.i).
+		then(res=>res.json()).
+		then(json=>this.members=json.results[0].members).
+		catch(error=>console.log("Se produjo un error"+ error))
+	}
+})
 
-// const app= new Vue{
-// 	el:"#app",
-// 	data:{
-// 		members:[]
-// 	}
-
-// }
-
-// Parte1
-let lista= document.getElementById('lista');
-async function getData() {
-		let api="https://api.propublica.org/congress/v1/113/senate/members.json";
-		let i={headers:{"X-API-Key":"cBmV3a8yZ4WsCVDLZmmTsPksTFKrLO4bwrqtt200"}};
-		let promise= await fetch(api,i);
-		let isOk= promise.ok ;
-		let result= isOk? await promise.json(): alert(promise.status);
-		// data.members=result.results[0].members;
-
-}
-getData()
 
 
 
